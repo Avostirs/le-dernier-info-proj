a4e0626185e44dac5a2a00108b5926b409541c0e
clem01120 <37347390+clem01120@users.noreply.github.com>
2018-03-19 09:56:59 +0100

    Add files via upload

---
+++ AppliChrono.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+
+public class AppliChrono {
+	  public static void main(String[] argv) {   
+	      JFrame fenetre = new JFrame();
+	      fenetre.setContentPane(new IHMChrono(Integer.parseInt(argv[0])));
+	      fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      fenetre.setLocation(100, 100);
+	      fenetre.pack();
+	      fenetre.setVisible(true);
+	    }
+	}
