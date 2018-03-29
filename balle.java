82065c57364679bcfce3e16bf95344817c60bac0
MarieRou <37419364+MarieRou@users.noreply.github.com>
2018-03-29 16:22:01 +0200

    Add files via upload

--- balle.java
+++ balle.java
@@ -55,8 +55,8 @@ public balle(canon moncanon){
 	b=true;
 	
 	//attributs largeur et longueur
-	l=moncanon.image2.getWidth()*0.5*0.01;
-	L=moncanon.image2.getHeight()*0.5*0.01;
+	l=moncanon.getimage().getWidth()*0.5*0.01;
+	L=moncanon.getimage().getHeight()*0.5*0.01;
 	
 	//attributs position
 	x=(l*Math.cos(Math.toRadians(alpha))+0.50)*Math.pow(10,2);
