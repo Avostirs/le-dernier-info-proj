edda7e0c36eb73a6a0f8b08f196dad0d49413a2a
MarieRou <37419364+MarieRou@users.noreply.github.com>
2018-03-29 16:54:31 +0200

    Add files via upload

--- FinDuJeu.java
+++ FinDuJeu.java
@@ -6,12 +6,12 @@ import java.awt.Color;
 
 public class FinDuJeu extends JFrame{
 	
-	protected JButton monbouton;
-	protected JLabel texte;
-	protected JLabel texte2;
+	private JButton monbouton;
+	private JLabel texte;
+	private JLabel texte2;
 	private int p;
 
-		
+	//Le constructeur prend en parametre k qui est le nombre de tirs a la fin du chrono	
 	public FinDuJeu(int k){
 		setSize(800,530);
 		setLocation(300,100);
@@ -19,28 +19,22 @@ public class FinDuJeu extends JFrame{
 		setVisible(false);
 		setTitle("fin");
 		this.p=k;
-	
-		
-		   
-		
-		    
-		   
-		    //zone de texte 
-			texte=new JLabel("Nombres de tirs : "+this.p+" !!");
-			texte.setBounds(100,50,600,310);
-			texte.setForeground(new Color(255,255,255));
-			Font font=new Font("Stencil", Font.BOLD, 25);
-			texte.setFont(font);
-			//zone de texte2
-			texte2=new JLabel("A Vos Tirs !");
-			texte2.setBounds(280,25,600,40);
-			texte2.setForeground(new Color(128,128,0));
-			Font font2=new Font("Stencil", Font.BOLD, 30);
-			texte2.setFont(font2);
+
+		//zone de texte 
+		texte=new JLabel("Nombres de tirs : "+this.p+" !!");
+		texte.setBounds(100,50,600,310);
+		texte.setForeground(new Color(255,255,255));
+		Font font=new Font("Stencil", Font.BOLD, 25);
+		texte.setFont(font);
+		//zone de texte2
+		texte2=new JLabel("A Vos Tirs !");
+		texte2.setBounds(280,25,600,40);
+		texte2.setForeground(new Color(128,128,0));
+		Font font2=new Font("Stencil", Font.BOLD, 30);
+		texte2.setFont(font2);
 			
 		    
-		    
-		    
+		      
 		//Affichage conteneur interieur
 		JPanel interieur =new JPanel();  
 		interieur.setBackground(new Color(200,173,127));
@@ -57,12 +51,10 @@ public class FinDuJeu extends JFrame{
 		bord.setLayout(null); 
 		this.setContentPane(bord); 
 		
-	
-		revalidate();
 		validate();
 		
 }	
-
+//methodes set
 public void setp(int k){
 	this.p = k;
 	
