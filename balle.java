c18cf069114be9095eb47c7796421b525e691e47
clem01120 <37347390+clem01120@users.noreply.github.com>
2018-04-02 11:53:23 +0200

    Add files via upload
    mise en méthodes

--- balle.java
+++ balle.java
@@ -78,10 +78,10 @@ public balle(canon moncanon){
 	}
 	this.setVisible(false);
 	repaint();
+
 }
 
-
-
+public void keyPressed(KeyEvent e){
 // Déplacement de la balle
 // Démarrage du timer lorsqu'on appuie sur la barre espace du clavier
 char carac = e.getKeyChar();
@@ -92,17 +92,20 @@ char carac = e.getKeyChar();
 	    b=false;
 	}
 }
+
+
 // Méthode keyReleased:
 public void keyReleased(KeyEvent e){
 	char carac = e.getKeyChar();
 	j = (int)(carac);
     	if (j==32 && b==false){
-		//Affichage de la balle
-		this.setVisible(true);
-		tempsSpace=System.currentTimeMillis()-startSpace;
+            //Affichage de la balle
+            this.setVisible(true);
+            tempsSpace=System.currentTimeMillis()-startSpace;
 	    	//Correction de l'erreur systematique
-		alpha=0.5*moncanon.getangle();//erreur systematique
-		repaint();	
+            alpha=0.5*moncanon.getangle();//erreur systematique
+            repaint();	
+        
 		if(!t1.isRunning()) {
 			t1.start();
 			startTime=System.currentTimeMillis();
@@ -115,15 +118,216 @@ public void keyReleased(KeyEvent e){
 		b=true;
 		this.s=false;
 	
-	}			
+        }			
 }
+
 //Méthode keyTyped
 public void keyTyped(KeyEvent e){}
 
 //Méthode actionPerformed
 public void actionPerformed(ActionEvent e){
 	// On incrémente vo en fonction du temps d'appui sur la barre espace
-	if(tempsSpace<200 &&tempsSpace>0){
+	deterv0();
+    
+	//temps ecoulé en secondes	
+	elapsedTime = (System.currentTimeMillis() - startTime)*0.001;
+	// Equations du PFD (on a pris comme échelle 1m=100 pixels)
+	trajectoireballe();	
+	repaint();	
+			
+}
+
+//Méthode paint
+public void paint(Graphics g){
+	
+	//Dessin de la balle
+	AffineTransform a = AffineTransform.getTranslateInstance(x,z);
+	a.scale(0.04,0.04);
+	Graphics2D g2 = (Graphics2D) g;
+	g2.drawImage(image,a,null);
+    
+    jaugev0(g,g2);
+	
+	//Dessin de la barre de puissance
+	//Affichage du cadre:
+	g2.setStroke(new BasicStroke(4));
+	g.setColor(Color.black);
+	g.drawRect(5,608,257,40);
+	g2.setStroke(new BasicStroke(2));
+    
+	Toolkit.getDefaultToolkit().sync();
+	repaint();
+	
+	
+}	
+    
+ 
+public void trajectoireballe(){
+    x=(vo*Math.cos(Math.toRadians(alpha))*elapsedTime+(l*Math.cos(Math.toRadians(alpha))+0.50))*Math.pow(10,2);
+    z=(0.5*g*Math.pow(elapsedTime,2)-vo*(Math.sin(Math.toRadians(alpha)))*elapsedTime+(4.50-L*Math.sin(Math.toRadians(alpha))))*Math.pow(10,2);
+}  
+
+public void jaugev0(Graphics g,Graphics g2){
+    
+	//affichage de la jauge en fonction de vo
+        if(vo>0 && vo<=5){
+		// Premier carré
+            g.setColor(Color.black);
+            g.drawRect(9,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(11,614,28,28);
+        }	
+        else if(vo>5 && vo<=7){
+		//Premier carré
+            g.setColor(Color.black);
+            g.drawRect(9,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(11,614,28,28);
+            //Deuxième carré	
+            g.setColor(Color.black);
+            g.drawRect(45,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(47,614,28,28);
+        }	
+        else if(vo>7 && vo<=9){
+            //Premier carré
+            g.setColor(Color.black);
+            g.drawRect(9,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(11,614,28,28);
+            //Deuxième carré	
+            g.setColor(Color.black);
+            g.drawRect(45,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(47,614,28,28);
+            //Troisème carré
+            g.setColor(Color.black);
+            g.drawRect(81,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(83,614,28,28);
+        }
+        else if(vo>9 && vo<=11){
+            //Premier carré
+            g.setColor(Color.black);
+            g.drawRect(9,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(11,614,28,28);
+            //Deuxième carré	
+            g.setColor(Color.black);
+            g.drawRect(45,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(47,614,28,28);
+            //Troisème carré
+            g.setColor(Color.black);
+            g.drawRect(81,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(83,614,28,28);
+            //Quatrième carré
+            g.setColor(Color.black);
+            g.drawRect(117,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(119,614,28,28);	
+        }
+        else if(vo>11 && vo<=12){
+            //Premier carré
+            g.setColor(Color.black);
+            g.drawRect(9,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(11,614,28,28);
+            //Deuxième carré	
+            g.setColor(Color.black);
+            g.drawRect(45,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(47,614,28,28);
+            //Troisème carré
+            g.setColor(Color.black);
+            g.drawRect(81,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(83,614,28,28);
+            //Quatrième carré
+            g.setColor(Color.black);
+            g.drawRect(117,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(119,614,28,28);
+            //Cinquième carré
+            g.setColor(Color.black);
+            g.drawRect(153,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(155,614,28,28);
+        }
+        else if(vo>12 && vo<=13){
+            //Premier carré
+            g.setColor(Color.black);
+            g.drawRect(9,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(11,614,28,28);
+            //Deuxième carré	
+            g.setColor(Color.black);
+            g.drawRect(45,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(47,614,28,28);
+            //Troisème carré
+            g.setColor(Color.black);
+            g.drawRect(81,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(83,614,28,28);
+            //Quatrième carré
+            g.setColor(Color.black);
+            g.drawRect(117,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(119,614,28,28);
+            //Cinquième carré
+            g.setColor(Color.black);
+            g.drawRect(153,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(155,614,28,28);
+            //Sixième carré
+            g.setColor(Color.black);
+            g.drawRect(189,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(191,614,28,28);
+        }
+        else if(vo>13 && vo<=14){
+            //Premier carré
+            g.setColor(Color.black);
+            g.drawRect(9,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(11,614,28,28);
+            //Deuxième carré	
+            g.setColor(Color.black);
+            g.drawRect(45,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(47,614,28,28);
+            //Troisème carré
+            g.setColor(Color.black);
+            g.drawRect(81,612,32,32);
+            g.setColor(Color.green);
+            g.fillRect(83,614,28,28);
+            //Quatrième carré
+            g.setColor(Color.black);
+            g.drawRect(117,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(119,614,28,28);
+            //Cinquième carré
+            g.setColor(Color.black);
+            g.drawRect(153,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(155,614,28,28);
+            //Sixième carré
+            g.setColor(Color.black);
+            g.drawRect(189,612,32,32);
+            g.setColor(Color.yellow);
+            g.fillRect(191,614,28,28);
+            //Septième carré
+            g.setColor(Color.black);
+            g.drawRect(225,612,32,32);
+            g.setColor(Color.red);
+            g.fillRect(227,614,28,28);
+        }
+}
+      
+ public void deterv0(){ 
+     if(tempsSpace<200 &&tempsSpace>0){
 		vo=5;
 	}else if(tempsSpace>=200 && tempsSpace<400){
 		vo=7;
@@ -138,196 +342,13 @@ public void actionPerformed(ActionEvent e){
 	}else {
 		vo=14;
 	}
-	
-	//temps ecoulé en secondes	
-	elapsedTime = (System.currentTimeMillis() - startTime)*0.001;
-	// Equations du PFD (on a pris comme échelle 1m=100 pixels)
-	x=(vo*Math.cos(Math.toRadians(alpha))*elapsedTime+(l*Math.cos(Math.toRadians(alpha))+0.50))*Math.pow(10,2);
-	z=(0.5*g*Math.pow(elapsedTime,2)-vo*(Math.sin(Math.toRadians(alpha)))*elapsedTime+(4.50-L*Math.sin(Math.toRadians(alpha))))*Math.pow(10,2);	
-	repaint();	
-			
-}
-
-//Méthode paint
-public void paint(Graphics g){
-	
-	//Dessin de la balle
-	AffineTransform a = AffineTransform.getTranslateInstance(x,z);
-	a.scale(0.04,0.04);
-	Graphics2D g2 = (Graphics2D) g;
-	g2.drawImage(image,a,null);
-	
-	//Dessin de la barre de puissance
-	//Affichage du cadre:
-	g2.setStroke(new BasicStroke(4));
-	g.setColor(Color.black);
-	g.drawRect(5,608,257,40);
-	g2.setStroke(new BasicStroke(2));
-	//affichage de la jauge en fonction de vo
-	if(vo>0 && vo<=5){
-		// Premier carré
-		g.setColor(Color.black);
-		g.drawRect(9,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(11,614,28,28);
-	}	
-	else if(vo>5 && vo<=7){
-		//Premier carré
-		g.setColor(Color.black);
-		g.drawRect(9,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(11,614,28,28);
-		//Deuxième carré	
-		g.setColor(Color.black);
-		g.drawRect(45,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(47,614,28,28);
-	}	
-	else if(vo>7 && vo<=9){
-		//Premier carré
-		g.setColor(Color.black);
-		g.drawRect(9,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(11,614,28,28);
-		//Deuxième carré	
-		g.setColor(Color.black);
-		g.drawRect(45,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(47,614,28,28);
-		//Troisème carré
-		g.setColor(Color.black);
-		g.drawRect(81,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(83,614,28,28);
-	}
-	else if(vo>9 && vo<=11){
-		//Premier carré
-		g.setColor(Color.black);
-		g.drawRect(9,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(11,614,28,28);
-		//Deuxième carré	
-		g.setColor(Color.black);
-		g.drawRect(45,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(47,614,28,28);
-		//Troisème carré
-		g.setColor(Color.black);
-		g.drawRect(81,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(83,614,28,28);
-		//Quatrième carré
-		g.setColor(Color.black);
-		g.drawRect(117,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(119,614,28,28);	
-	}
-	else if(vo>11 && vo<=12){
-		//Premier carré
-		g.setColor(Color.black);
-		g.drawRect(9,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(11,614,28,28);
-		//Deuxième carré	
-		g.setColor(Color.black);
-		g.drawRect(45,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(47,614,28,28);
-		//Troisème carré
-		g.setColor(Color.black);
-		g.drawRect(81,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(83,614,28,28);
-		//Quatrième carré
-		g.setColor(Color.black);
-		g.drawRect(117,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(119,614,28,28);
-		//Cinquième carré
-		g.setColor(Color.black);
-		g.drawRect(153,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(155,614,28,28);
-	}
-	else if(vo>12 && vo<=13){
-		//Premier carré
-		g.setColor(Color.black);
-		g.drawRect(9,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(11,614,28,28);
-		//Deuxième carré	
-		g.setColor(Color.black);
-		g.drawRect(45,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(47,614,28,28);
-		//Troisème carré
-		g.setColor(Color.black);
-		g.drawRect(81,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(83,614,28,28);
-		//Quatrième carré
-		g.setColor(Color.black);
-		g.drawRect(117,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(119,614,28,28);
-		//Cinquième carré
-		g.setColor(Color.black);
-		g.drawRect(153,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(155,614,28,28);
-		//Sixième carré
-		g.setColor(Color.black);
-		g.drawRect(189,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(191,614,28,28);
-	}
-	else if(vo>13 && vo<=14){
-		//Premier carré
-		g.setColor(Color.black);
-		g.drawRect(9,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(11,614,28,28);
-		//Deuxième carré	
-		g.setColor(Color.black);
-		g.drawRect(45,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(47,614,28,28);
-		//Troisème carré
-		g.setColor(Color.black);
-		g.drawRect(81,612,32,32);
-		g.setColor(Color.green);
-		g.fillRect(83,614,28,28);
-		//Quatrième carré
-		g.setColor(Color.black);
-		g.drawRect(117,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(119,614,28,28);
-		//Cinquième carré
-		g.setColor(Color.black);
-		g.drawRect(153,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(155,614,28,28);
-		//Sixième carré
-		g.setColor(Color.black);
-		g.drawRect(189,612,32,32);
-		g.setColor(Color.yellow);
-		g.fillRect(191,614,28,28);
-		//Septième carré
-		g.setColor(Color.black);
-		g.drawRect(225,612,32,32);
-		g.setColor(Color.red);
-		g.fillRect(227,614,28,28);
-	}
-	Toolkit.getDefaultToolkit().sync();
-	repaint();
-	}
-	
-	
+ }
+ 
+     
 //Méthodes get
 public double get_centre_x(){
 	return x+15;
 }
-
 public double get_centre_z(){
 	return z+15;
 }
